975d80d68c1d64be4606d19d0da62b91e7d77b76
charlesgobina <charles.gobina@gmail.com>
2022-04-03 22:34:49 +0100

    Add card to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Card from './components/card/Card';
 import './App.css';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Card />
   </div>
 );
 
