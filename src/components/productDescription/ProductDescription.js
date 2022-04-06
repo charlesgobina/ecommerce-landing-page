dbfa76b4098a93b83b30064a1731933740513721
charlesgobina <charles.gobina@gmail.com>
2022-04-06 23:56:21 +0100

    Build product description overview

---
+++ src/components/productDescription/ProductDescription.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDescription = () => {
+  return (
+    <div className="product-description">
+      <p>Product description goes here</p>
+    </div>
+  )
+}
+
+export default ProductDescription;
