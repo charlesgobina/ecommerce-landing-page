1a527930acb6fab39a3a128099e208153af057a5
charlesgobina <charles.gobina@gmail.com>
2022-04-06 23:58:29 +0100

    Fix linter errors

--- src/components/productDescription/ProductDescription.js
+++ src/components/productDescription/ProductDescription.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const ProductDescription = () => {
-  return (
-    <div className="product-description">
-      <p>Product description goes here</p>
-    </div>
-  )
-}
+const ProductDescription = () => (
+  <div className="product-description">
+    <p>Product description goes here</p>
+  </div>
+);
 
 export default ProductDescription;
