dcf400cddd3e4fb8db8fea79d2474f49bb0f24c7
charlesgobina <charles.gobina@gmail.com>
2022-06-12 23:30:59 +0100

    Dispatch item to redux store and set message property

--- src/redux/sneakStore.js
+++ src/redux/sneakStore.js
@@ -1,17 +1,28 @@
 const ADD_ITEM = 'itemStore/ADD_ITEM';
+const SEND_MESSAGE = 'itemStore/SEND_MESSAGE';
 
-const initialState = [];
+const initialState = {
+  item: [],
+  messageShow: false,
+};
 
 export const addItem = (payload) => ({
   type: ADD_ITEM,
   payload,
 });
 
+export const showMessage = (payload) => ({
+  type: SEND_MESSAGE,
+  payload,
+});
+
 const reducer = (state = initialState, action) => {
   if (action.type === ADD_ITEM) {
-    return [...state, action.payload];
+    return { item: [...state.item, action.payload] };
+  } if (action.type === SEND_MESSAGE) {
+    return { ...state, messageShow: action.payload };
   }
-  return null;
+  return state;
 };
 
 export default reducer;
