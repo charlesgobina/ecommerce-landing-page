027420588db07114b53942fab2e7d8016ee43cf0
charlesgobina <charles.gobina@gmail.com>
2022-06-13 16:04:38 +0100

    Remove message show

--- src/redux/sneakStore.js
+++ src/redux/sneakStore.js
@@ -1,9 +1,7 @@
 const ADD_ITEM = 'itemStore/ADD_ITEM';
-const SEND_MESSAGE = 'itemStore/SEND_MESSAGE';
 
 const initialState = {
   item: [],
-  messageShow: false,
 };
 
 export const addItem = (payload) => ({
@@ -11,16 +9,9 @@ export const addItem = (payload) => ({
   payload,
 });
 
-export const showMessage = (payload) => ({
-  type: SEND_MESSAGE,
-  payload,
-});
-
 const reducer = (state = initialState, action) => {
   if (action.type === ADD_ITEM) {
     return { item: [...state.item, action.payload] };
-  } if (action.type === SEND_MESSAGE) {
-    return { ...state, messageShow: action.payload };
   }
   return state;
 };
