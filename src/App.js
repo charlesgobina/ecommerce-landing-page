bd612786dafef82af9f8dd913a4a7df6e1b946fe
charlesgobina <charles.gobina@gmail.com>
2022-06-13 19:48:35 +0100

    Add click state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import Card from './components/card/Card';
 import ProductDescription from './components/productDescription/ProductDescription';
 import Cart from './components/cart/Cart';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Card />
-    <ProductDescription />
-    <Cart />
-  </div>
-);
+const App = () => {
+  const [detectClick, setDetectClick] = useState(false);
+
+  return (
+    <div className="App">
+      <Header setDetectClick={setDetectClick} />
+      <Cart detectClick={detectClick} />
+      <Card />
+      <ProductDescription />
+    </div>
+  );
+};
 
 export default App;
